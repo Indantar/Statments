e05e69a0b16b5edd0ea09a525c90482164e878b6
Dawid Kotwicki <dawid.kotwicki@pl01.microgen.group>
2018-09-17 21:52:37 +0200

    Initial commit

---
+++ src/com/DawidKotwicki/Main.java
@@ -0,0 +1,17 @@
+package com.DawidKotwicki;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myVariable = 50;
+	    myVariable++;
+	    myVariable--;
+        System.out.println("This is a statment");
+        System.out.println("This is " +
+                "another "+
+                "statment. ");
+
+        int varA = 5; varA--;
+        
+    }
+}
